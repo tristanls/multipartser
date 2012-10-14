45e30eb381bb02410a5eda1b6ce8272df612ab20
Tristan Slominski <tristan.slominski@gmail.com>
2012-10-13 22:06:46 -0500

    Initial commit of working multipartser

---
+++ README.md
@@ -0,0 +1,92 @@
+multipartser
+====
+
+`mutipartser` is an evented multipart form submission parser.
+
+## Installation
+
+    npm install multipartser
+
+## Usage
+
+Below gets complicated, but so does multipart parsing. Please let me know if you find any are errors.
+
+The first half just extracts the boundary from the `Content-Type` header. The second half is the one that actually does the parsing.
+
+```javascript
+var http = require( 'http' ),
+    multipartser = require( 'multipartser' );
+
+http.createServer( function ( req, res ) {
+
+  // get boundary from 'Content-Type' header
+  var contentType = req.headers && req.headers[ 'Content-Type' ];
+  if ( ! contentType ) {
+    res.writeHead( 400 ); // Missing Content-Type
+    res.end();
+    return;
+  }
+  
+  var contentTypeParts = contentType.split( ';' );
+  if ( contentTypeParts.length != 2 ) {
+    res.writeHead( 400 );
+    res.end();
+    return;
+  }
+
+  contentType = contentTypeParts[ 0 ];
+
+  if ( contentType != 'multipart/form-data' ) {
+    res.writeHead( 415 ); // Unsupported Media Type
+    res.end();
+    return;
+  }
+
+  var boundary = contentTypeParts[ 1 ];
+  boundary = boundary.trim().split( '=' );
+
+  if ( boundary.length != 2 ) {
+    res.writeHead( 400 ); // Missing multipart boundary
+    res.end();
+    return;
+  }
+
+  boundary = boundary[ 1 ];
+
+  var parser = multipartser(); // finally.. geesh
+  parser.boundary( boundary );
+
+  parser.on( 'part', function ( part ) {
+
+    if ( part.type == 'file' ) {
+      
+      console.log( 'type', part.type );
+      console.log( 'name', part.name );
+      console.log( 'filename', part.filename );
+      console.log( 'contents', part.contents );
+      console.log( 'contentType', part.contentType );
+
+    } else if ( part.type == 'field' ) {
+
+      console.log( 'type', part.type );
+      console.log( 'name', part.name );
+      console.log( 'value', part.value );
+
+    }
+
+  }); // parser.on 'part'
+
+  parser.on( 'end', function () {
+    console.log( 'completed parsing' );
+  }); // parser.on 'end'
+
+  parser.on( 'error', function ( error ) {
+    console.error( error );
+  }); // parser.on 'error'
+
+  req.setEncoding( 'utf8' );
+  req.on( 'data', parser.data );
+  req.on( 'end', parser.end );
+
+}).listen( 1337, '127.0.0.1' );
+```
---
+++ index.js
@@ -0,0 +1,165 @@
+/* 
+ * index.js : Evented multipart parser
+ *
+ * (C) 2012 Tristan Slominski
+ */
+
+var events = require( 'events' );
+
+//
+// ## function multipartser ()
+// Multipartser factory that returns a multipartser event emitter.
+//
+var multipartser = function multipartser () {
+
+  var _boundary,
+      body = '';
+      emitter = new events.EventEmitter();
+
+  // set boundary for parsing
+  emitter.boundary = function boundary ( __boundary ) {
+    
+    _boundary = __boundary;
+    return emitter;
+
+  }; // emitter.boundary
+
+  // parse data
+  emitter.data = function data ( data ) {
+
+    body += data;
+
+    // see if body contains a full part
+    var splits = body.split( "--" + _boundary );
+
+    // nothing to do if no full parts found yet
+    if ( splits.length < 3 ) { return; }
+
+    // we found at least one part
+    // the first element should be empty string indicating that 
+    // boundary was at the beginning of the body
+    if ( splits[ 0 ] != '' ) {
+      emitter.emit( 'error', { noBoundaryAtBodyBeginning : true } );
+      return;
+    }
+
+    var parts = [];
+
+    // grab all splits except the first and last one
+    for ( var i = 1; i < splits.length - 1; i++ ) {
+      parts.push( splits[ i ] );
+    }
+
+    // check if we are done, or if there is more stuff coming
+    var lastSplit = splits[ splits.length - 1 ];
+
+    if ( lastSplit != '--' && lastSplit != '--\r\n' ) {
+
+      // we are not finished, change body to contain just the remaining part
+      body = "--" + _boundary + lastSplit;
+
+    } else {
+      body = undefined;
+    }    
+
+    emitter.emit( 'parts', parts );
+    return emitter;
+
+  }; // emitter.data
+
+  emitter.end = function end () {
+
+    if ( typeof( body ) !== 'undefined' ) {
+      emitter.emit( 'error', { unparsedBody : true, body : body } );
+    }
+
+    emitter.emit( 'end' );
+    return emitter;
+
+  }; // emitter.end
+
+  // parse the multipart parts
+  emitter.on( 'parts', function ( parts ) {
+
+    parts.forEach( function ( part ) {
+
+      // part should start with \r\n
+      if ( part.indexOf( "\r\n" ) != 0 ) {
+        emitter.emit( 'error', { missingRNAtPartBeginning : true, part : part } );
+        return;
+      }
+
+      // trim leading \r\n
+      part = part.slice( 2 );
+
+      // part should end with \r\n
+      if ( part.substring( part.length - 2 ) != "\r\n" ) {
+        emitter.emit( 'error', { missingRNAtPartEnding: true } );
+        return;
+      }
+
+      // trim trailing \r\n
+      part = part.slice( 0, part.length - 2 );
+
+      // 'part' now contains part content only
+      // get Content-Disposition line
+      var rnIndex = part.indexOf( "\r\n" );
+      var contentDisposition = part.slice( 0, rnIndex );
+      var contentType = null;
+
+      // trim Content-Disposition line
+      part = part.slice( rnIndex + 2 );
+
+      // if next line starts with Content-Type and not \r\n, we have a file
+      if ( part.indexOf( "Content-Type" ) == 0 ) {
+
+        rnIndex = part.indexOf( "\r\n" );
+        contentType = part.slice( 0, rnIndex );
+        // trim Content-Type line
+        part = part.slice( rnIndex + 2 );
+
+      } // if ( part.indexOf( "Content-Type" ) == 0 )
+
+      // trim \r\n separator between headers and part contents
+      part = part.slice( 2 );
+
+      var name = grab( contentDisposition, 'name' );
+      var filename = grab( contentDisposition, 'filename' );
+
+      if ( contentType ) {
+
+        var capture = contentType.match( /^Content-Type:\s*(.*)/ );
+
+        if ( ! capture ) {
+          emitter.emit( 'error', { missingContentType : true } );
+          return;
+        }
+
+        contentType = capture[ 1 ].trim();
+
+      } // if ( contentType )
+
+      if ( filename && contentType ) {
+
+        // we have a file
+        emitter.emit( 'part', { type : 'file', name : name, filename : filename,
+          contents : part, contentType : contentType } );
+        return;
+
+      } else {
+
+        // we have a value
+        emitter.emit( 'part', { type : 'field', name : name, value : part } );
+        return;
+
+      } // else
+
+    }); // parts.forEach
+
+  }); // emitter.on 'parts'
+
+  return emitter;
+
+}; // multipartser
+
+module.exports = multipartser;
